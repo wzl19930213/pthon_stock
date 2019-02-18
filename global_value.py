723517599bae9507e7a362877777650ecd31dc8f
e <1194352768@qq.com>
2019-02-18 23:09:22 +0800

    完善趋势策略

--- global_value.py
+++ global_value.py
@@ -1,2 +1,8 @@
 local_position = 'F:/python/pthon_stock/data/'
 old_data_position = 'F:/python/trading-data.20171107/stock_data'
+
+HEADER_DATE = 'date'
+HEADER_CLOSE = 'close'
+HEADER_HIGH = 'high'
+HEADER_LOW = 'low'
+HEADER_VOLUME = 'volume'
--- strategy/strategy_1.py
+++ strategy/strategy_1.py
@@ -11,8 +11,7 @@ import update_stock_code
 
 local_position = global_value.local_position
 daily_data_position = global_value.local_position + 'daily_data/'
-current_time = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-
+current_time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
 if os.path.exists(local_position + 'stock_basics.csv'):
     stock_basics = pd.read_csv(local_position + 'stock_basics.csv', encoding='GBK')
@@ -25,12 +24,13 @@ length = len(codes)
 
 print(current_time + '  code length: ' + str(length))
 count = 0
-codes = ['002500']
+strategy_high_code = []
+strategy_low_code = []
 for code in codes:
-    try:
-        print('%.2f%%' % (count/length*100))
-        count = count + 1
+    print('%.2f%%' % (count / length * 100))
+    count = count + 1
 
+    try:
         code = str(code).zfill(6)
         filename = daily_data_position + code + '.csv'
         if os.path.exists(filename):
@@ -39,8 +39,54 @@ for code in codes:
             i = 0
             for row in csv_data:
                 i = i + 1
-                print(row)
+                data_header = row
+                if i == 1:
+                    break
+            close_index = 0
+            date_index = 0
+            high_index = 0
+            low_index = 0
+            temp_close_index = 0
+            for name in data_header:
+                if name == global_value.HEADER_CLOSE:
+                    close_index = temp_close_index
+                if name == global_value.HEADER_DATE:
+                    date_index = temp_close_index
+                if name == global_value.HEADER_HIGH:
+                    high_index = temp_close_index
+                if name == global_value.HEADER_LOW:
+                    low_index = temp_close_index
+                temp_close_index = temp_close_index + 1
+            closePrice = []
+            highPrice = []
+            lowPrice = []
+            dateTime = []
+            for row in csv_data:
+                closePrice.append(row[close_index])
+                highPrice.append(row[high_index])
+                lowPrice.append(row[low_index])
+                dateTime.append(row[date_index])
+            highestPoint = []
+            highestDatePoint = []
+            lowestPoint = []
+            lowestDatePoint = []
+            for i in range(0, len(dateTime)):
+                if i == 0 or i == len(highPrice) - 1:
+                    continue
+                if i > 60:
+                    break
+                if highPrice[i] > highPrice[i + 1] and highPrice[i] > highPrice[i - 1]:
+                    highestPoint.append(highPrice[i])
+                    highestDatePoint.append(dateTime[i])
+                if lowPrice[i] < lowPrice[i + 1] and lowPrice[i] < lowPrice[i - 1]:
+                    lowestPoint.append(lowPrice[i])
+                    lowestDatePoint.append(dateTime[i])
+            if closePrice[1] < max(highestPoint) < closePrice[0]:
+                strategy_high_code.append(code)
+            if closePrice[1] > max(lowestPoint) > closePrice[0]:
+                strategy_low_code.append(code)
     except:
         print('error')
 print('100%')
-
+print(strategy_high_code)
+print(strategy_low_code)
